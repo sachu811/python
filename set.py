0eb5ca49e885916481a614911e4513db7703bb49
sachidanand dubey <sachidanand@subcodevs.com>
2024-02-19 14:29:45 +0530

    Set in python

---
+++ set.py
@@ -0,0 +1,12 @@
+# set is a mutable collection of data that does not allow any duplication
+# sets are basically used to include membership testing and eliminating duplicate entries.
+#indexing not allowed 
+#duplictated data not allowed 
+
+set1={1,2,3,4,5}
+print(set1)
+set1.add(99) #add numbers
+print(set1)
+print(len(set1)) #length count
+set1.remove(99)
+print(set1) #remove
