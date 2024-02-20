5a9e20bdaefdfefe03f054fb49affac290311d60
sachidanand dubey <sachidanand@subcodevs.com>
2024-02-21 00:02:46 +0530

    variable details

---
+++ variable.py
@@ -0,0 +1,34 @@
+#all are acceptable
+    # myvar = "John"
+    # my_var = "John"
+    # _my_var = "John"
+    # myVar = "John"
+    # MYVAR = "John"
+    # myvar2 = "John"
+
+#not acceptable
+    # 2myvar="Jhon"
+    # my-var="Jhon"
+    # my var="Jhon"
+
+
+# a=5
+# b="text"
+# print(a)
+# print(b)
+
+#ACCESSING MULTIPLE VARIABLE
+# a,b,c='apple','orange','banana'
+# print(a)
+# print(b)
+# print(c)
+# a=b=c='orange'
+# print(a)
+# print(b)
+# print(c)
+
+fruits=['apple','mango','banana'] #list
+a,b,c=fruits
+print(a)
+print(b)
+print(c)
