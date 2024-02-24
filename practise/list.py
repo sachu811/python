4a5c5fe068624986c6f512b61e005f028360e1c6
sachidanand dubey <sachidanand@subcodevs.com>
2024-02-24 10:34:58 +0530

    practice

---
+++ practise/list.py
@@ -0,0 +1,8 @@
+List=[]
+sub1=input("enter first subject: ")
+sub2=input("enter second subject: ")
+sub3=input("enter third subject: ")
+List.append(sub1)
+List.append(sub2)
+List.append(sub3)
+print(List)
